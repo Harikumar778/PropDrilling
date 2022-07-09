5c0c3f4c665e93708f267792fdcca2d93c061f4d
Harry <67307171+Harikumar778@users.noreply.github.com>
2022-07-09 20:02:28 +0530

    'commit'

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,6 @@
+import { useNavigate } from "react-router-dom";
 function Cart({ cart, setCart }) {
+  const navigate = useNavigate();
   return (
     <div>
       {cart.map((ele) => (
@@ -6,8 +8,13 @@ function Cart({ cart, setCart }) {
           <img src={ele.imageURL} alt="" height="100px" width="100px" />
           <span>{ele.name}</span>
           <span>{ele.price}</span>
+          <button onClick={() => setCart(cart.filter((c) => c.id !== ele.id))}>
+            Remove from Cart
+          </button>
         </div>
       ))}
+
+      <button onClick={() => navigate(-1)}>Go Back Home</button>
     </div>
   );
 }
